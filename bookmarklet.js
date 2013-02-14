d379107343b1c9a7e5a06db14490107c4f0693f4
Max <max@teamatb.com>
2013-02-13 22:48:37 -0600

    Sending selections to the server now.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,13 @@
 javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.9.1",function($,L){
-	console.log('loaded.');	
-	$('body').empty();
-	$.getJSON('http://localhost:4567/test?callback=?', {test: ['oh', 'please', 'work']}, function(data) {
+	console.log('loaded.');
+
+	var selection = document.getSelection().toString();
+	console.log(selection);
+	var array = [];
+	array = selection.split(/[ ,.?;:"']+/);
+	console.log(array);
+
+	$.getJSON('http://localhost:4567/test?callback=?', {test: array}, function(data) {
 		alert(data);
 		console.log('maderequest');
 	});
