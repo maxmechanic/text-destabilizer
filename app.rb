26221cab7db9981161d9c8a7a10a5cd0150048db
Max <max@teamatb.com>
2013-02-14 00:31:34 -0600

    Building out text analysis and parser.

--- app.rb
+++ app.rb
@@ -4,11 +4,37 @@ require 'json'
 require 'pp'
 require 'sinatra/jsonp'
 
-index = WordNet::NounIndex.instance
+$nouns = WordNet::NounIndex.instance
+$adj = WordNet::AdjectiveIndex.instance
+$verb = WordNet::VerbIndex.instance
+$adv = WordNet::AdverbIndex.instance
 
-def getWord(word, index)
-	lemma = index.find(word)
-	lemma.synsets.each { |synset| pp synset.words }
+
+
+
+def get_synonyms(word)
+
+	synonyms = nil
+	lemma = $nouns.find(word)
+	lemma = $adj.find(word) unless lemma
+	lemma = $verb.find(word) unless lemma
+	lemma = $adv.find(word) unless lemma
+	pp lemma
+	# lemma.synsets.each { |synset| pp synset.words } unless !lemma
+	synonyms = lemma.synsets.map { |synset| synset.words } unless !lemma
+	pp synonyms
+	return synonyms
+end
+
+def analyze_text(words)
+	words.each do |word|
+		pp word
+		synonyms = get_synonyms(word)
+		pp synonyms
+		# word['synonyms'] = synonyms unless !synonyms
+		# word['synonyms'] unless !synonyms
+	end
+
 end
 
 # getWord('pants', index)
@@ -21,8 +47,8 @@ end
 
 get '/test' do
 
-	pp params['test']
-
+	words = params['test']
+	analyze_text(words)
 	data = ["hello","hi","hallo"]
 
 	JSONP data
